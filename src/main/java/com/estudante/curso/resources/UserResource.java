ef6a0a82f6ac0ae5e43b4e4a0ff95b0ae3499e9d
Carlos-Siebra <carlosarmandosiebra@gmail.com>
2023-08-30 18:20:11 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/estudante/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudante.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudante.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmai.com", "9999999", "2563");
+		return ResponseEntity.ok().body(u);
+	}
+}
