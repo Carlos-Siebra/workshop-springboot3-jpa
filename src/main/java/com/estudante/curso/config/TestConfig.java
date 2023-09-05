2ea2524e89d1d65348b53fd415240d73f315f023
Carlos-Siebra <carlosarmandosiebra@gmail.com>
2023-09-05 20:49:26 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/estudante/curso/config/TestConfig.java
+++ src/main/java/com/estudante/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudante.curso.entities.Category;
 import com.estudante.curso.entities.Order;
 import com.estudante.curso.entities.OrderItem;
+import com.estudante.curso.entities.Payment;
 import com.estudante.curso.entities.Product;
 import com.estudante.curso.entities.User;
 import com.estudante.curso.entities.enuns.OrderStatus;
@@ -82,5 +83,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
